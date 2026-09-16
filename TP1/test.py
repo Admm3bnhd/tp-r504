8920e5e14d0a3967698992a3d2e3107531fd1685
Admm3.bnhd <adamm.bnhd@gmail.com>
2026-09-16 14:50:36 +0200

    feat: test qui couvre les cas limites

--- TP1/test.py
+++ TP1/test.py
@@ -19,4 +19,18 @@ def test_3():
 
 def test_exc_1():
     with pytest.raises(Exception):
-        f.puissance(0,-1)
+        f.puissance(0,-1)    
+    with pytest.raises(Exception):
+        f.puissance(0, -5)
+
+
+def test_cas_limites():
+    # x^0 = 1 pour x != 0
+    assert f.puissance(2, 0) == 1
+    assert f.puissance(-2, 0) == 1
+
+    # Puissances positives
+    assert f.puissance(1, 100) == 1
+    assert f.puissance(-1, 4) == 1
+    assert f.puissance(-1, 5) == -1
+
