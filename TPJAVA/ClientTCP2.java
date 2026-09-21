914a076a4aa7e83221138c49609c1d99c246c7b4
Admm3.bnhd <adamm.bnhd@gmail.com>
2026-09-21 13:46:03 +0200

    finished: Serveur TCP2 & ClientTCP2

---
+++ TPJAVA/ClientTCP2.java
@@ -0,0 +1,22 @@
+import java.net.*;
+import java.io.*;
+
+public class ClientTCP2
+{
+	public static void main(String[] args)
+	{
+	//ClientTCP1.java
+	try
+	{
+		Socket socket = new Socket("localhost",2016);
+		DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+		dOut.writeUTF(args[0]);
+		socket.close();
+	}
+	catch(Exception ex)
+	{
+		System.out.println("erreur !");
+		ex.printStackTrace();
+	}
+	}
+}
---
+++ TPJAVA/ServeurTCP2.java
@@ -0,0 +1,28 @@
+import java.net.*;
+import java.io.*;
+
+public class ServeurTCP2
+{
+	public static void main(String[] args)
+	{
+	//ServeurTCP1.java
+	try
+	{
+		ServerSocket socketserver = new ServerSocket(2016);
+		System.out.println("serveur en attente");
+		while(true)
+		{
+			Socket socket = socketserver.accept();
+			System.out.println("connection d'un client");
+			DataInputStream dIn = new DataInputStream(socket.getInputStream());
+			System.out.println("message: " + dIn.readUTF());
+		}
+	}
+	catch(Exception ex)
+	{
+		System.out.println("erreur !");
+		ex.printStackTrace();
+	}
+	}
+}
+
